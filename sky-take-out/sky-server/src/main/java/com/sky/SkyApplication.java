f0ab12b42d60fe53980986a362c83383d5268550
99-WSJ <sjwang_er@163.com>
2024-01-17 21:55:20 +0800

    test

--- sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -12,6 +12,7 @@ public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         // test
+
         log.info("server started");
     }
 }
